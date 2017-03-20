4a8b2fb2c4636219335f72098eab79b90c367ae8
yandiandian <643761053@qq.com>
2017-03-20 13:59:13 +0800

    20170320

--- app/src/main/java/com/example/funspotrs/activity/ReceiveActivity.java
+++ app/src/main/java/com/example/funspotrs/activity/ReceiveActivity.java
@@ -1,15 +1,31 @@
 package com.example.funspotrs.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.example.funspotrs.R;
+import com.example.funspotrs.common.ActivityUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ReceiveActivity extends AppCompatActivity {
 
+    @BindView(R.id.btn_sure)
+    Button btnSure;
+private ActivityUtils activityUtils;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
+        ButterKnife.bind(this);
+        activityUtils=new ActivityUtils(this);
+    }
+
+    @OnClick(R.id.btn_sure)
+    public void onClick() {
+        activityUtils.startActivity(ResumeLoadActivity.class);
     }
 }
--- app/src/main/java/com/example/funspotrs/me/MeFragment.java
+++ app/src/main/java/com/example/funspotrs/me/MeFragment.java
@@ -7,9 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.funspotrs.R;
 import com.example.funspotrs.common.ActivityUtils;
+import com.pkmmte.view.CircularImageView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -18,23 +22,45 @@ import butterknife.OnClick;
  */
 public class MeFragment extends Fragment {
 
+    @BindView(R.id.iv_user_head)
+    CircularImageView ivUserHead;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.tv_invitenb)
+    TextView tvInvitenb;
+    @BindView(R.id.tv_person_info)
+    TextView tvPersonInfo;
+    @BindView(R.id.tv_person_goods)
+    TextView tvPersonGoods;
     private View view;
     private ActivityUtils activityUtils;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if (view == null){
-            view = inflater.inflate(R.layout.fragment_me,container,false);
-            ButterKnife.bind(this,view);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_me, container, false);
+            ButterKnife.bind(this, view);
             activityUtils = new ActivityUtils(this);
         }
+        ButterKnife.bind(this, view);
         return view;
     }
 
-    @OnClick({R.id.iv_user_head,R.id.tv_person_info, R.id.tv_login})
+
+    @OnClick({R.id.iv_user_head, R.id.tv_username, R.id.tv_invitenb, R.id.tv_person_info, R.id.tv_person_goods})
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.iv_user_head:
+                break;
+            case R.id.tv_username:
+                break;
+            case R.id.tv_invitenb:
+                break;
+            case R.id.tv_person_info:
+                break;
+            case R.id.tv_person_goods:
+                break;
+        }
     }
-
 }
