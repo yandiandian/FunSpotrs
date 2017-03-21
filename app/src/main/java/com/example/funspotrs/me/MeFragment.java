6fce13be1de889d0385a5738ef98b94e699afbea
yandiandian <643761053@qq.com>
2017-03-21 11:08:15 +0800

    20170321

--- app/src/main/java/com/example/funspotrs/me/MeFragment.java
+++ app/src/main/java/com/example/funspotrs/me/MeFragment.java
@@ -21,46 +21,10 @@ import butterknife.OnClick;
  * A simple {@link Fragment} subclass.
  */
 public class MeFragment extends Fragment {
-
-    @BindView(R.id.iv_user_head)
-    CircularImageView ivUserHead;
-    @BindView(R.id.tv_username)
-    TextView tvUsername;
-    @BindView(R.id.tv_invitenb)
-    TextView tvInvitenb;
-    @BindView(R.id.tv_person_info)
-    TextView tvPersonInfo;
-    @BindView(R.id.tv_person_goods)
-    TextView tvPersonGoods;
-    private View view;
-    private ActivityUtils activityUtils;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if (view == null) {
-            view = inflater.inflate(R.layout.fragment_me, container, false);
-            ButterKnife.bind(this, view);
-            activityUtils = new ActivityUtils(this);
-        }
-        ButterKnife.bind(this, view);
-        return view;
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-
-    @OnClick({R.id.iv_user_head, R.id.tv_username, R.id.tv_invitenb, R.id.tv_person_info, R.id.tv_person_goods})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.iv_user_head:
-                break;
-            case R.id.tv_username:
-                break;
-            case R.id.tv_invitenb:
-                break;
-            case R.id.tv_person_info:
-                break;
-            case R.id.tv_person_goods:
-                break;
-        }
+        return inflater.inflate(R.layout.fragment_me, container, false);
     }
 }
