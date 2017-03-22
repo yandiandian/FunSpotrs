742295e847dac06dc935f4c37e125f4e790e942f
yandiandian <643761053@qq.com>
2017-03-22 10:36:28 +0800

    20170322

--- app/src/main/java/com/example/funspotrs/course/CourseFragment.java
+++ app/src/main/java/com/example/funspotrs/course/CourseFragment.java
@@ -6,25 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.funspotrs.R;
+import com.example.funspotrs.adapter.CourseAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CourseFragment extends Fragment {
-
-
-    public CourseFragment() {
-        // Required empty public constructor
-    }
-
-
+    @BindView(R.id.list_view)
+    ListView listView;
+    private CourseAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course, container, false);
+        View view = inflater.inflate(R.layout.fragment_course, container, false);
+        ButterKnife.bind(this, view);
+        listView.setAdapter(adapter);
+        return view;
+
     }
 
 }
